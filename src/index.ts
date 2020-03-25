e7baa706dd30583398f33112619586b95782fb36
kayryu <lukayryu@gmail.com>
2020-03-25 09:28:18 +0800

    add files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+
+
+/**
+
+#. how to create instance?
+let api = new BoolApi({option});
+
+#. how to use?
+    * state
+    api.getName()
+    api.get...() not better
+
+    api.tx.send()
+
+ */
